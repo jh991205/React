f22456fb3794c0c4193432d9f61927c0369e3b53
jh991205 <jiadihuang19991205@hotmail.com>
2023-04-09 12:52:26 -0400

    effects

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  // State
   const [apiData, setApiData] = useState({});
-
-  // API KEY AND URL
   const apiKey = process.env.REACT_APP_API_KEY;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${'shanghai'}&appid=${apiKey}`;
 
-  // Side effect
   useEffect(() => {
     fetch(apiUrl)
       .then((res) => res.json())
