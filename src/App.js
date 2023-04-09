fea50cb70b9c7014082994c34543adc818ae9a9b
jh991205 <jiadihuang19991205@hotmail.com>
2023-04-09 12:51:36 -0400

    data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // State
+  const [apiData, setApiData] = useState({});
+
+  // API KEY AND URL
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${'shanghai'}&appid=${apiKey}`;
+
+  // Side effect
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => setApiData(data));
+  }, [apiUrl]);
+
+  console.log(apiData)
+
   return (
     <div className="App">
       
